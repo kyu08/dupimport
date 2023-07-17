71c3fd2cb4a199c80a5051477a91387c68c959b8
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-07-17 13:46:07 +0900

    init

---
+++ dupimport_test.go
@@ -0,0 +1,15 @@
+package dupimport_test
+
+import (
+	"testing"
+
+	"github.com/dupimport"
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.Run(t, testdata, dupimport.Analyzer, "a")
+}
---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/dupimport
+
+go 1.19
+
